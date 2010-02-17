76d8ffd1cce06f2b6cba2151df530d839bb2837e
dnolen <david.nolen@gmail.com>
2010-02-17 13:10:45 -0500

    Initial commit

---
+++ src/native_deps/core.clj
@@ -0,0 +1,21 @@
+(ns native-deps.core
+  (:require [lancet])
+  (:use [leiningen.pom :only [default-repos]]
+        [leiningen.deps :only [make-repository make-dependency]]
+        [clojure.contrib.java-utils :only [file]])
+  (:import [org.apache.maven.artifact.ant DependenciesTask]))
+
+(defn native-deps
+  [project]
+  (let [deps-task (DependenciesTask.)]
+    (.setFilesetId deps-task "native-dependency.fileset")
+    (.setProject deps-task lancet/ant-project)
+    (.setPathId deps-task (:name project))
+    (doseq [r (map make-repository (concat default-repos
+                                           (:repositories project)))]
+      (.addConfiguredRemoteRepository deps-task r))
+    (doseq [dep (:native-dependencies project)]
+      (.addDependency deps-task (make-dependency dep)))
+    (.execute deps-task)
+    (lancet/unjar {:dest ""} (.getReference lancet/ant-project
+                                            (.getFilesetId deps-task)))))
---
+++ test/native_deps/core_test.clj
@@ -0,0 +1,6 @@
+(ns native-deps.core-test
+  (:use [native-deps.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
