b94986aa0d1fa12231223d3dd856bfb88dfbb49e
dnolen <david.nolen@gmail.com>
2010-02-28 18:11:14 -0500

    * README: adding usage instructions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen/native-deps "1.0.0-SNAPSHOT"
+(defproject native-deps "1.0.0"
   :description "Leiningen plugin to get native dependencies."
   :dependencies [[org.apache.maven/maven-ant-tasks "2.0.10"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
