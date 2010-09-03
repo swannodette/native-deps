e7c488d02337f12f8aeb484a39c6d145e3f0e366
David Nolen <david.nolen@gmail.com>
2010-09-03 08:43:47 -0400

    fixes the lein deps bug

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject native-deps "1.0.3"
+(defproject native-deps "1.0.4"
   :description "Leiningen plugin to get native dependencies."
-  :dependencies [[org.apache.maven/maven-ant-tasks "2.0.10"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
+  :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.3.0-SNAPSHOT"]])
