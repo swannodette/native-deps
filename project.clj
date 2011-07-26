7101dc23546a660c7d8406d9a2b253a25d30049a
Ian Phillips <ianp@ianp.org>
2011-07-27 01:09:05 +0200

    Bumped deps to latest versions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject native-deps "1.0.5"
+(defproject native-deps "1.0.6"
   :description "Leiningen plugin to get native dependencies."
-  :dev-dependencies [[lein-clojars "0.6.0"]
-                     [swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[lein-clojars "0.7.0"]
+                     [swank-clojure "1.4.0-SNAPSHOT"]])
